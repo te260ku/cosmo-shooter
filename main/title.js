92e5c22d05b2053f26ef7f6548c4845e94feba9f
tetsu <teturou@aw.wakwak.com>
2020-01-01 15:37:37 +0900

    fix style of title page

---
+++ main/title.js
@@ -0,0 +1,12 @@
+// var closeButton = document.getElementById("close");
+// var blocker = document.getElementById("blocker");
+// closeButton.addEventListener("click", close, false);
+
+
+
+// function close() {
+//    // blocker.style.display = "none";
+//    $("#blocker").css("display", "none");
+//    // blocker.innerHTML = "";
+//    console.log("pushed");
+// }
