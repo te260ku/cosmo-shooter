730ab588d4dfb23417016586acbfe867b6cd8101
te26Oku <teturou@aw.wakwak.com>
2020-01-12 20:47:26 +0900

    add movement of enemies

--- main/script.js
+++ main/script.js
@@ -3,14 +3,17 @@ var camera;
 var renderer;
 var controls;
 var emitter;
+var width = window.innerWidth;
+var height = window.innerHeight;
+
 
 // フィールド関連
-var cubes = [];
-var cubeNum = 3;
-var cubePositionX = [10, 8, 12];
-var cubePositionY = [1, 5, 2];
-var cubePositionZ = [0, 10, 10];
-var cubePositions = {};
+var spheres = [];
+var sphereNum = 4;
+var spherePositionX = [10, 8, 12, 0];
+var spherePositionY = [1, 5, 2, 0];
+var spherePositionZ = [0, 10, 10, 0];
+var spherePositions = {};
 
 // ゲームシステム関連
 var time = 0;
@@ -18,6 +21,23 @@ var limitTime = 100;
 var score = 0;
 var keyboard = {};
 var endFlag = false;
+const min_speed = 0.1;
+const max_speed = 0.5;
+const sphere_width = 10;
+const sphere_height = 10;
+let x = [];
+    let y = [];
+    let z = [];
+    let dx = [];
+    let dy = [];
+    let dz = [];
+    const min_pos_x = -(Number(width) / 2);
+    const max_pos_x = (Number(width) / 2);
+    const min_pos_y = -(Number(height) / 2);
+    const max_pos_y = (Number(height) / 2);
+    const min_pos_z = -(Number(height) / 2);
+    const max_pos_z = (Number(height) / 2);
+
 
 // UI関連
 var blocker = document.getElementById('blocker');
@@ -73,38 +93,40 @@ function init() {
 
 
    // フィールドの生成
-   const geometry_f = new THREE.SphereGeometry(100, 100, 100);
-   geometry_f.scale(-1, 1, 1);
 
-   //テクスチャ画像を読み込み
-   const loader_f = new THREE.TextureLoader();
-   const texture_f = loader_f.load('test.jpg');
+   // // 360
+   // const geometry_f = new THREE.SphereGeometry(100, 100, 100);
+   // geometry_f.scale(-1, 1, 1);
 
-   //球体のマテリアルを生成
-   const material_f = new THREE.MeshBasicMaterial({
-     map: texture_f
-   });
+   // //テクスチャ画像を読み込み
+   // const loader_f = new THREE.TextureLoader();
+   // const texture_f = loader_f.load('test.jpg');
 
-   //球体を生成
-   const sphere_f = new THREE.Mesh(geometry_f, material_f);
+   // //球体のマテリアルを生成
+   // const material_f = new THREE.MeshBasicMaterial({
+   //   map: texture_f
+   // });
 
-   scene.add(sphere_f);
+   // //球体を生成
+   // const sphere_f = new THREE.Mesh(geometry_f, material_f);
+
+   // scene.add(sphere_f);
 
 
    
-   // var backgroundGeo = new THREE.SphereGeometry(1000, 90, 45);
-   // // var textureLoader = new THREE.TextureLoader();
-   // // backgroundTex = textureLoader.load("background.jpeg");
+   var backgroundGeo = new THREE.SphereGeometry(1000, 90, 45);
+   // var textureLoader = new THREE.TextureLoader();
+   // backgroundTex = textureLoader.load("background.jpeg");
    
-   // var backgroundMat = new THREE.MeshBasicMaterial({
-   //    color: "gray",
-   //    // color:0xffffff, 
-   //    wireframe: true, 
-   //    // map: backgroundTex
-   // })
+   var backgroundMat = new THREE.MeshBasicMaterial({
+      color: "gray",
+      // color:0xffffff, 
+      wireframe: true, 
+      // map: backgroundTex
+   })
 
-   // const background = new THREE.Mesh(backgroundGeo, backgroundMat);
-   // scene.add(background);
+   const background = new THREE.Mesh(backgroundGeo, backgroundMat);
+   scene.add(background);
 
    // デバッグ用
    const axes = new THREE.AxisHelper(400);
@@ -152,32 +174,57 @@ function init() {
 
    // ----敵の生成------------------------------------
 
-   // var cubeGeo = new THREE.BoxGeometry(1, 1, 1);
-   var cubeGeo = new THREE.SphereGeometry();
-   // var cubeMat = new THREE.MeshPhongMaterial({color:0x5555ff, wireframe:false});
-   var cubeMat = new THREE.MeshNormalMaterial({ wireframe: false });
+   // var sphereGeo = new THREE.BoxGeometry(1, 1, 1);
+   var sphereGeo = new THREE.SphereGeometry(50, sphere_width, sphere_height);
+   // var sphereGeo = new THREE.SphereGeometry();
+   // var sphereMat = new THREE.MeshPhongMaterial({color:0x5555ff, wireframe:false});
+   var sphereMat = new THREE.MeshNormalMaterial({ wireframe: false });
 
    // var shader = FresnelShader;
 
    
-   for (i = 0; i < cubeNum; i++) {
-      cube = new THREE.Mesh(cubeGeo, cubeMat);
+   // すべて生成してシーンに追加
+   for (var i = 0; i < sphereNum; i++) {
+      sphere = new THREE.Mesh(sphereGeo, sphereMat);
+      if (sphere.geometry.boundingSphere == null) {
+         sphere.geometry.computeBoundingSphere();
+      }
+      sphere.geometry.boundingSphere.radius *= 1.4;
+      sphere.name = "sphere-" + [i];
+      spheres.push(sphere);
+      scene.add(sphere);
+   }
 
-      // 位置はvector3で指定する
-      cube.position.x = cubePositionX[i];
-      cube.position.y = cubePositionY[i];
-      cube.position.z = cubePositionZ[i];
+
+
+    for (var i = 0; i < sphereNum; i++) {
+      x.push(Math.floor(Math.random() * (max_pos_x + 1 - min_pos_x)) + min_pos_x);
+      y.push(Math.floor(Math.random() * (max_pos_y + 1 - min_pos_y)) + min_pos_y);
+      z.push(Math.floor(Math.random() * (max_pos_z + 1 - min_pos_z)) + min_pos_z);
+      dx.push(Math.floor(Math.random() * (max_speed + 1 - min_speed)) + min_speed);
+      dy.push(Math.floor(Math.random() * (max_speed + 1 - min_speed)) + min_speed);
+      dz.push(Math.floor(Math.random() * (max_speed + 1 - min_speed)) + min_speed);
+  }
+
+   
+   // for (i = 0; i < sphereNum; i++) {
+   //    sphere = new THREE.Mesh(sphereGeo, sphereMat);
+
+   //    // 位置はvector3で指定する
+   //    sphere.position.x = spherePositionX[i];
+   //    sphere.position.y = spherePositionY[i];
+   //    sphere.position.z = spherePositionZ[i];
       
 
-      if (cube.geometry.boundingSphere == null) {
-         cube.geometry.computeBoundingSphere();
-      }
-      //少しだけ小さめにする
-      cube.geometry.boundingSphere.radius *= 0.8;
-      cube.name = "cube-" + [i];
-      cubes.push(cube);
-      scene.add(cube);
-   }
+   //    if (sphere.geometry.boundingSphere == null) {
+   //       sphere.geometry.computeBoundingSphere();
+   //    }
+   //    //少しだけ小さめにする
+   //    sphere.geometry.boundingSphere.radius *= 0.8;
+   //    sphere.name = "sphere-" + [i];
+   //    spheres.push(sphere);
+   //    scene.add(sphere);
+   // }
 
    // -------------------------------------------------
 
@@ -192,13 +239,13 @@ var canShoot = true;
 
 function onMouseDown() {
    // shotAudio.play();
-   let bullet = new THREE.Mesh(new THREE.SphereGeometry(0.5, 8, 4), new THREE.MeshBasicMaterial({
+   let bullet = new THREE.Mesh(new THREE.SphereGeometry(2, 8, 4), new THREE.MeshBasicMaterial({
       color: "aqua"
    }));
    if (bullet.geometry.BoundingSphere == null) {
       bullet.geometry.computeBoundingSphere();
    }
-   bullet.geometry.boundingSphere.radius *= 0.8;
+   bullet.geometry.boundingSphere.radius *= 1.2;
    // 始点 - 銃の先
    bullet.position.copy(emitter.getWorldPosition()); 
    bullet.quaternion.copy(camera.quaternion); 
@@ -220,7 +267,7 @@ function onMouseDown() {
 }
 
 // 弾丸のスピード
-var speed = 50;
+var speed = 100;
 // 時間管理
 var clock = new THREE.Clock();
 var delta = 0;
@@ -234,10 +281,39 @@ function render() {
       onMouseDown();
    }
 
-   // sphereを動かしているところ
-   step += 0.01;
-   cubes[0].position.z = 10 + (10*(Math.cos(step)));
-   cubes[0].position.y = 1.5 + (10*Math.abs(Math.sin(step)));
+   // // sphereを動かしているところ
+   // step += 0.01;
+   // spheres[0].position.z = 10 + (10*(Math.cos(step)));
+   // spheres[0].position.y = 1.5 + (10*Math.abs(Math.sin(step)));
+
+
+   // 動かす
+   for (var i in spheres) {
+      x[i] += dx[i];
+      y[i] += dy[i];
+      z[i] += dz[i];
+      if (x[i] > (width / 2 - (sphere_width / 2)) || x[i] < (-width / 2 + (sphere_width / 2))) {
+          dx[i] = -dx[i];
+          x[i] += dx[i];
+      }
+      if (y[i] > (height / 2 - (sphere_height / 2)) || y[i] < (-height / 2 + (sphere_height / 2))) {
+          dy[i] = -dy[i];
+          y[i] += dy[i];
+      }
+      if (z[i] > (height / 2 - (sphere_height / 2)) || z[i] < (-height / 2 + (sphere_height / 2))) {
+          dz[i] = -dz[i];
+          z[i] += dz[i];
+      }
+
+      spheres[i].position.x = x[i];
+      spheres[i].position.y = y[i];
+      spheres[i].position.z = z[i];
+  };
+
+
+
+
+
 
    // controlsのオンオフ
    if (keyboard[81]) {
@@ -289,23 +365,23 @@ function render() {
 
 
 
-      for (i = 0; i < cubes.length; i++) {
+      for (i = 0; i < spheres.length; i++) {
 
-         var targetCube = cubes[i].geometry.boundingSphere.clone();
-         targetCube.applyMatrix4(cubes[i].matrixWorld);
+         var targetsphere = spheres[i].geometry.boundingSphere.clone();
+         targetsphere.applyMatrix4(spheres[i].matrixWorld);
 
-         if (targetCube.intersectsSphere(targetBullet)) {
+         if (targetsphere.intersectsSphere(targetBullet)) {
             // StruckAudio.play();
             score++;
             scoreLabel.innerHTML = score;
             console.log("hit");
 
-            t = scene.getObjectByName(cubes[i].name);
+            t = scene.getObjectByName(spheres[i].name);
 
             scene.remove(t);
 
-            scene.remove(targetCube);
-            cubes.splice(i, 1);
+            scene.remove(targetsphere);
+            spheres.splice(i, 1);
 
             scene.remove(targetBullet);
             scene.remove(b);
